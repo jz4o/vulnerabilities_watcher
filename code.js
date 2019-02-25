d82de4e8f2d93ff13bd816d7817a9d5ab7f13e15
jz4o <26317360+jz4o@users.noreply.github.com>
2019-02-25 21:50:27 +0900

    スプレッドシートの初期化処理を実装

--- code.js
+++ code.js
@@ -1,4 +1,105 @@
-function myFunction() {
+/**
+ * グローバル変数.
+ *
+ * spreadSheet [SpreadSheet] スプレッドシート
+ * configSheet [Sheet]       configシート
+ * config      [HashMap]     configシートの内容
+ */
+var spreadSheet = SpreadsheetApp.getActiveSpreadsheet();
+var configSheet;
+var config;
 
+// セットアップ.
+setup();
+
+/**
+ * 各シートおよびconfigの初期化処理.
+ */
+function setup() {
+  setupSheets();
+  setupConfig();
+}
+
+/**
+ * シートの初期化処理.
+ *
+ * 不足分シートを追加し、グローバル変数にセット
+ */
+function setupSheets() {
+  // シート名
+  var sheetNames = ['config'];
+
+  for (var i = 0; i < sheetNames.length; i++) {
+    var sheetName = sheetNames[i];
+
+    // シート追加
+    if (spreadSheet.getSheetByName(sheetName) == null) {
+      spreadSheet.insertSheet(sheetName);
+    }
+
+    // グローバル変数にセット
+    eval(sheetName + 'Sheet = spreadSheet.getSheetByName("' + sheetName + '")')
+  }
+}
+
+/**
+ * 設定値の初期化処理.
+ *
+ * 不足分設定を追加し、configシートを更新
+ */
+function setupConfig() {
+  // configシートの内容を取得
+  config = getConfig();
+
+  // 設定内容のデフォルト値
+  var configDefaultValues = {
+    'latestWatchedAt': new Date(0)
+  }
+
+  // 設定されていない項目にデフォルト値をセット
+  for (key in configDefaultValues) {
+    if (config[key] == null) {
+      config[key] = configDefaultValues[key];
+    }
+  }
+
+  // configシートの内容を更新
+  updateConfigSheet();
+}
+
+/**
+ * configシートの内容を取得し、呼び出し元に返す.
+ *
+ * @return [HashMap] configシートの内容
+ */
+function getConfig() {
+  var result = {};
+
+  var configData  = configSheet.getDataRange().getValues();
+  var titleRow    = configData[0];
+  var keyColumn   = titleRow.indexOf('key');
+  var valueColumn = titleRow.indexOf('value');
+
+  for (var i = 1; i < configData.length; i++) {
+    var key = configData[i][keyColumn];
+    var value = configData[i][valueColumn];
+    result[key] = value;
+  }
+
+  return result;
+}
+
+/**
+ * configシートの内容を更新.
+ */
+function updateConfigSheet() {
+  configSheet.clear();
+  var data = [];
+  data.push(['key', 'value'])
+  for (key in config) {
+    data.push([key, config[key]]);
+  }
+
+  configSheet.getRange(1, 1, data.length, 2).setValues(data);
 }
 
