7024112882f392aaa3748bb4d9a18d486654850a
jz4o <26317360+jz4o@users.noreply.github.com>
2019-10-14 12:01:21 +0900

    テストコードの構成を変更

--- test/redmine_test.js
+++ test/redmine_test.js
@@ -20,180 +20,182 @@ function redmineTest() {
   };
 
   exports({
-    'createTicketForWhenNotFoundNewVulnerability': function() {
-      // override function to mock
-      createTicket = createTicketMock;
+    'redmine': {
+      'createTicketForWhenNotFoundNewVulnerability': function() {
+        // override function to mock
+        createTicket = createTicketMock;
 
-      var siteName  = 'testSite';
-      var watchedAt = new Date();
+        var siteName  = 'testSite';
+        var watchedAt = new Date();
 
-      var result = JSON.stringify(createTicketForWhenNotFoundNewVulnerability(siteName, watchedAt));
-      var expect = JSON.stringify({
-        'subject':     buildTicketSubject(siteName, watchedAt, null),
-        'description': '',
-        'statusId':    redmine['status']['resolve'],
-        'categoryId':  redmine['category']['vulnerabilityNothing'],
-        'doneRatio':   100
-      });
+        var result = JSON.stringify(createTicketForWhenNotFoundNewVulnerability(siteName, watchedAt));
+        var expect = JSON.stringify({
+          'subject':     buildTicketSubject(siteName, watchedAt, null),
+          'description': '',
+          'statusId':    redmine['status']['resolve'],
+          'categoryId':  redmine['category']['vulnerabilityNothing'],
+          'doneRatio':   100
+        });
 
-      assertThat(result).is(expect);
+        assertThat(result).is(expect);
 
-      // revert overridden function
-      createTicket = createTicketOrigin;
-    },
-    'createTicketForWatchOver': function() {
-      // override function to mock
-      createTicket = createTicketMock;
+        // revert overridden function
+        createTicket = createTicketOrigin;
+      },
+      'createTicketForWatchOver': function() {
+        // override function to mock
+        createTicket = createTicketMock;
 
-      var siteName           = 'testSite';
-      var watchedAt          = new Date();
-      var vulnerabilityTitle = 'testTitle';
-      var vulnerabilityLink  = 'testLink'
-
-      var result = JSON.stringify(createTicketForWatchOver(siteName, watchedAt, vulnerabilityTitle, vulnerabilityLink));
-      var expect = JSON.stringify({
-        'subject':     buildTicketSubject(siteName, watchedAt, vulnerabilityTitle),
-        'description': vulnerabilityLink,
-        'statusId':    redmine['status']['resolve'],
-        'categoryId':  redmine['category']['watchOver'],
-        'doneRatio':   100
-      });
-
-      assertThat(result).is(expect);
-
-      // revert overridden function
-      createTicket = createTicketOrigin;
-    },
-    'createTicketForEscalation': function() {
-      // override function to mock
-      createTicket = createTicketMock;
-
-      var siteName           = 'testSite';
-      var watchedAt          = new Date();
-      var vulnerabilityTitle = 'testTitle';
-      var vulnerabilityLink  = 'testLink'
-
-      var result = JSON.stringify(createTicketForEscalation(siteName, watchedAt, vulnerabilityTitle, vulnerabilityLink));
-      var expect = JSON.stringify({
-        'subject':     buildTicketSubject(siteName, watchedAt, vulnerabilityTitle),
-        'description': vulnerabilityLink,
-        'statusId':    redmine['status']['new'],
-        'categoryId':  redmine['category']['escalation'],
-        'doneRatio':   0
-      });
-
-      assertThat(result).is(expect);
-
-      // revert overridden function
-      createTicket = createTicketOrigin;
-    },
-    'buildTicketSubject': {
-      'when vulnerability title is specified': function() {
-        var sitename           = 'testSite';
-        var watchedAt          = new Date(0);
+        var siteName           = 'testSite';
+        var watchedAt          = new Date();
         var vulnerabilityTitle = 'testTitle';
+        var vulnerabilityLink  = 'testLink'
 
-        var result = buildTicketSubject(sitename, watchedAt, vulnerabilityTitle)
-        var expect = 'testSite 1970-01-01 09:00 [testTitle]';
+        var result = JSON.stringify(createTicketForWatchOver(siteName, watchedAt, vulnerabilityTitle, vulnerabilityLink));
+        var expect = JSON.stringify({
+          'subject':     buildTicketSubject(siteName, watchedAt, vulnerabilityTitle),
+          'description': vulnerabilityLink,
+          'statusId':    redmine['status']['resolve'],
+          'categoryId':  redmine['category']['watchOver'],
+          'doneRatio':   100
+        });
 
         assertThat(result).is(expect);
+
+        // revert overridden function
+        createTicket = createTicketOrigin;
       },
-      'when vulnerability title is not specified': function() {
-        var sitename           = 'testSite';
-        var watchedAt          = new Date(0);
-        var vulnerabilityTitle = null;
+      'createTicketForEscalation': function() {
+        // override function to mock
+        createTicket = createTicketMock;
 
-        var result = buildTicketSubject(sitename, watchedAt, vulnerabilityTitle)
-        var expect = 'testSite 1970-01-01 09:00';
+        var siteName           = 'testSite';
+        var watchedAt          = new Date();
+        var vulnerabilityTitle = 'testTitle';
+        var vulnerabilityLink  = 'testLink'
+
+        var result = JSON.stringify(createTicketForEscalation(siteName, watchedAt, vulnerabilityTitle, vulnerabilityLink));
+        var expect = JSON.stringify({
+          'subject':     buildTicketSubject(siteName, watchedAt, vulnerabilityTitle),
+          'description': vulnerabilityLink,
+          'statusId':    redmine['status']['new'],
+          'categoryId':  redmine['category']['escalation'],
+          'doneRatio':   0
+        });
 
         assertThat(result).is(expect);
-      }
-    },
-    'createTicket': function() {
-      // override Object#getContentText to mock
-      var getContentTextOrigin = Object.prototype.getContentText;
-      Object.prototype.getContentText = function() {
-        return JSON.stringify({ 'issue': this });
-      };
 
-      var subject     = 'testTitle';
-      var description = 'testDescription';
-      var statusId    = 999;
-      var categoryId  = 888;
-      var doneRatio   = 100;
+        // revert overridden function
+        createTicket = createTicketOrigin;
+      },
+      'buildTicketSubject': {
+        'when vulnerability title is specified': function() {
+          var sitename           = 'testSite';
+          var watchedAt          = new Date(0);
+          var vulnerabilityTitle = 'testTitle';
 
-      var result = JSON.stringify(createTicket(subject, description, statusId, categoryId, doneRatio));
-      var expect = JSON.stringify({
-        'url':    (redmine['url'] + '/issues.json'),
-        'params': {
-          'method'      : 'post',
-          'contentType' : 'application/json',
-          "headers"     : {
-            'X-Redmine-API-Key': redmine['apiKey']
-          },
-          'payload'     : JSON.stringify({
-            "issue": {
-              "project_id"     : redmine['projectId'],
-              "tracker_id"     : redmine['tracker']['task'],
-              "subject"        : subject,
-              "description"    : description,
-              "status_id"      : statusId,
-              "priority_id"    : redmine['priority']['normal'],
-              "assigned_to_id" : config['watcherRedmineId'],
-              "category_id"    : categoryId,
-              "done_ratio"     : doneRatio
-            }
-          })
+          var result = buildTicketSubject(sitename, watchedAt, vulnerabilityTitle)
+          var expect = 'testSite 1970-01-01 09:00 [testTitle]';
+
+          assertThat(result).is(expect);
+        },
+        'when vulnerability title is not specified': function() {
+          var sitename           = 'testSite';
+          var watchedAt          = new Date(0);
+          var vulnerabilityTitle = null;
+
+          var result = buildTicketSubject(sitename, watchedAt, vulnerabilityTitle)
+          var expect = 'testSite 1970-01-01 09:00';
+
+          assertThat(result).is(expect);
         }
-      });
-
-      assertThat(result).is(expect);
-
-      // revert overridden function
-      Object.prototype.getContentText = getContentTextOrigin;
-    },
-    'getTicketId': {
-      'when ticket exist': function() {
-        // override function to mock
+      },
+      'createTicket': function() {
+        // override Object#getContentText to mock
         var getContentTextOrigin = Object.prototype.getContentText;
         Object.prototype.getContentText = function() {
-          return JSON.stringify({
-            'total_count': 1,
-            'results': [
-              { 'id': 999 }
-            ]
-          });
-        }
+          return JSON.stringify({ 'issue': this });
+        };
 
-        var vulnerabilityLink = 'testLink';
+        var subject     = 'testTitle';
+        var description = 'testDescription';
+        var statusId    = 999;
+        var categoryId  = 888;
+        var doneRatio   = 100;
 
-        var result = getTicketId(vulnerabilityLink);
-        var expect = 999;
+        var result = JSON.stringify(createTicket(subject, description, statusId, categoryId, doneRatio));
+        var expect = JSON.stringify({
+          'url':    (redmine['url'] + '/issues.json'),
+          'params': {
+            'method'      : 'post',
+            'contentType' : 'application/json',
+            "headers"     : {
+              'X-Redmine-API-Key': redmine['apiKey']
+            },
+            'payload'     : JSON.stringify({
+              "issue": {
+                "project_id"     : redmine['projectId'],
+                "tracker_id"     : redmine['tracker']['task'],
+                "subject"        : subject,
+                "description"    : description,
+                "status_id"      : statusId,
+                "priority_id"    : redmine['priority']['normal'],
+                "assigned_to_id" : config['watcherRedmineId'],
+                "category_id"    : categoryId,
+                "done_ratio"     : doneRatio
+              }
+            })
+          }
+        });
 
         assertThat(result).is(expect);
 
         // revert overridden function
         Object.prototype.getContentText = getContentTextOrigin;
       },
-      'when ticket not exist': function() {
-        // override function to mock
-        var getContentTextOrigin = Object.prototype.getContentText;
-        Object.prototype.getContentText = function() {
-          return JSON.stringify({
-            'total_count': 0,
-            'results':     []
-          });
+      'getTicketId': {
+        'when ticket exist': function() {
+          // override function to mock
+          var getContentTextOrigin = Object.prototype.getContentText;
+          Object.prototype.getContentText = function() {
+            return JSON.stringify({
+              'total_count': 1,
+              'results': [
+                { 'id': 999 }
+              ]
+            });
+          }
+
+          var vulnerabilityLink = 'testLink';
+
+          var result = getTicketId(vulnerabilityLink);
+          var expect = 999;
+
+          assertThat(result).is(expect);
+
+          // revert overridden function
+          Object.prototype.getContentText = getContentTextOrigin;
+        },
+        'when ticket not exist': function() {
+          // override function to mock
+          var getContentTextOrigin = Object.prototype.getContentText;
+          Object.prototype.getContentText = function() {
+            return JSON.stringify({
+              'total_count': 0,
+              'results':     []
+            });
+          }
+
+          var vulnerabilityLink = 'testLink';
+
+          var result = getTicketId(vulnerabilityLink);
+          var expect = null;
+
+          assertThat(result).is(expect);
+
+          // revert overridden function
+          Object.prototype.getContentText = getContentTextOrigin;
         }
-
-        var vulnerabilityLink = 'testLink';
-
-        var result = getTicketId(vulnerabilityLink);
-        var expect = null;
-
-        assertThat(result).is(expect);
-
-        // revert overridden function
-        Object.prototype.getContentText = getContentTextOrigin;
       }
     }
   });
--- test/utilities_test.js
+++ test/utilities_test.js
@@ -3,120 +3,122 @@ function utilitiesTest() {
   var dateInt = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
 
   exports({
-    'isHoliday': {
-      'saturday': function() {
-        var dayDiff = dateInt.indexOf('saturday') - new Date(currentYear + '/06/01').getDay();
-        var targetDay = 1 + dayDiff + (dayDiff < 0 ? 14 : 7);
-        assertThat(isHoliday(new Date(currentYear + '/06/' + targetDay))).isTrue();
-      },
-      'sunday': function() {
-        var dayDiff = dateInt.indexOf('sunday') - new Date(currentYear + '/06/01').getDay();
-        var targetDay = 1 + dayDiff + (dayDiff < 0 ? 14 : 7);
-        assertThat(isHoliday(new Date(currentYear + '/06/' + targetDay))).isTrue();
-      },
-      'public holiday': {
-        'new years day': function() {
-           assertThat(isHoliday(new Date(currentYear + '/01/01'))).isTrue();
-        },
-        'adult day': function() {
-          // 2nd monday of january
-          var dayDiff = dateInt.indexOf('monday') - new Date(currentYear + '/01/01').getDay();
+    'utilities': {
+      'isHoliday': {
+        'saturday': function() {
+          var dayDiff = dateInt.indexOf('saturday') - new Date(currentYear + '/06/01').getDay();
           var targetDay = 1 + dayDiff + (dayDiff < 0 ? 14 : 7);
-          assertThat(isHoliday(new Date(currentYear + '/01/' + targetDay))).isTrue();
+          assertThat(isHoliday(new Date(currentYear + '/06/' + targetDay))).isTrue();
         },
-        'foundation day': function() {
-          assertThat(isHoliday(new Date(currentYear + '/02/11'))).isTrue();
+        'sunday': function() {
+          var dayDiff = dateInt.indexOf('sunday') - new Date(currentYear + '/06/01').getDay();
+          var targetDay = 1 + dayDiff + (dayDiff < 0 ? 14 : 7);
+          assertThat(isHoliday(new Date(currentYear + '/06/' + targetDay))).isTrue();
         },
-        'emperors birthday': function() {
-          assertThat(isHoliday(new Date(currentYear + '/02/23'))).isTrue();
-        },
-        'vernal equinox day': function() {
-          var targetDay = Math.floor(20.8431 + 0.242194 * (currentYear - 1980)) - Math.floor((currentYear - 1980) / 4);
-          assertThat(isHoliday(new Date(currentYear + '/03/' + targetDay))).isTrue();
-        },
-        'showa day': function() {
-          assertThat(isHoliday(new Date(currentYear + '/04/29'))).isTrue();
-        },
-        'constitution day': function() {
-          assertThat(isHoliday(new Date(currentYear + '/05/03'))).isTrue();
-        },
-        'green day': function() {
-          assertThat(isHoliday(new Date(currentYear + '/05/04'))).isTrue();
-        },
-        'childrens day': function() {
-          assertThat(isHoliday(new Date(currentYear + '/05/05'))).isTrue();
-        },
-        'sea day': function() {
-          if (currentYear == 2020) {
-            // for tokyo olympics special measures law
-            assertThat(isHoliday(new Date('2020/07/23'))).isTrue();
-          } else {
-            // 3rd monday of july
-            var dayDiff = dateInt.indexOf('monday') - new Date(currentYear + '/07/01').getDay();
-            var targetDay = 1 + dayDiff + (dayDiff < 0 ? 21 : 14);
-            assertThat(isHoliday(new Date(currentYear + '/07/' + targetDay))).isTrue();
-          }
-        },
-        'mountain day': function() {
-          if (currentYear == 2020) {
-            // for tokyo olympics special measures law
-            assertThat(isHoliday(new Date('2020/08/10'))).isTrue();
-          } else {
-            assertThat(isHoliday(new Date(currentYear + '/08/11'))).isTrue();
-          }
-        },
-        'respect for the aged day': function() {
-          // 3rd monday of september
-          var dayDiff = dateInt.indexOf('monday') - new Date(currentYear + '/09/01').getDay();
-          var targetDay = 1 + dayDiff + (dayDiff < 0 ? 21 : 14);
-          assertThat(isHoliday(new Date(currentYear + '/09/' + targetDay))).isTrue();
-        },
-        'equinox day': function() {
-          var targetDay = Math.floor(23.2488 + 0.242194 * (currentYear - 1980)) - Math.floor((currentYear - 1980) / 4);
-          assertThat(isHoliday(new Date(currentYear + '/09/' + targetDay))).isTrue();
-        },
-        'physical education day': function() {
-          if (currentYear == 2020) {
-            // for tokyo olympics special measures law
-            assertThat(isHoliday(new Date('2020/07/24'))).isTrue();
-          } else {
-            // 2nd monday of october
-            var dayDiff = dateInt.indexOf('monday') - new Date(currentYear + '/10/01').getDay();
+        'public holiday': {
+          'new years day': function() {
+            assertThat(isHoliday(new Date(currentYear + '/01/01'))).isTrue();
+          },
+          'adult day': function() {
+            // 2nd monday of january
+            var dayDiff = dateInt.indexOf('monday') - new Date(currentYear + '/01/01').getDay();
             var targetDay = 1 + dayDiff + (dayDiff < 0 ? 14 : 7);
-            assertThat(isHoliday(new Date(currentYear + '/10/' + targetDay))).isTrue();
+            assertThat(isHoliday(new Date(currentYear + '/01/' + targetDay))).isTrue();
+          },
+          'foundation day': function() {
+            assertThat(isHoliday(new Date(currentYear + '/02/11'))).isTrue();
+          },
+          'emperors birthday': function() {
+            assertThat(isHoliday(new Date(currentYear + '/02/23'))).isTrue();
+          },
+          'vernal equinox day': function() {
+            var targetDay = Math.floor(20.8431 + 0.242194 * (currentYear - 1980)) - Math.floor((currentYear - 1980) / 4);
+            assertThat(isHoliday(new Date(currentYear + '/03/' + targetDay))).isTrue();
+          },
+          'showa day': function() {
+            assertThat(isHoliday(new Date(currentYear + '/04/29'))).isTrue();
+          },
+          'constitution day': function() {
+            assertThat(isHoliday(new Date(currentYear + '/05/03'))).isTrue();
+          },
+          'green day': function() {
+            assertThat(isHoliday(new Date(currentYear + '/05/04'))).isTrue();
+          },
+          'childrens day': function() {
+            assertThat(isHoliday(new Date(currentYear + '/05/05'))).isTrue();
+          },
+          'sea day': function() {
+            if (currentYear == 2020) {
+              // for tokyo olympics special measures law
+              assertThat(isHoliday(new Date('2020/07/23'))).isTrue();
+            } else {
+              // 3rd monday of july
+              var dayDiff = dateInt.indexOf('monday') - new Date(currentYear + '/07/01').getDay();
+              var targetDay = 1 + dayDiff + (dayDiff < 0 ? 21 : 14);
+              assertThat(isHoliday(new Date(currentYear + '/07/' + targetDay))).isTrue();
+            }
+          },
+          'mountain day': function() {
+            if (currentYear == 2020) {
+              // for tokyo olympics special measures law
+              assertThat(isHoliday(new Date('2020/08/10'))).isTrue();
+            } else {
+              assertThat(isHoliday(new Date(currentYear + '/08/11'))).isTrue();
+            }
+          },
+          'respect for the aged day': function() {
+            // 3rd monday of september
+            var dayDiff = dateInt.indexOf('monday') - new Date(currentYear + '/09/01').getDay();
+            var targetDay = 1 + dayDiff + (dayDiff < 0 ? 21 : 14);
+            assertThat(isHoliday(new Date(currentYear + '/09/' + targetDay))).isTrue();
+          },
+          'equinox day': function() {
+            var targetDay = Math.floor(23.2488 + 0.242194 * (currentYear - 1980)) - Math.floor((currentYear - 1980) / 4);
+            assertThat(isHoliday(new Date(currentYear + '/09/' + targetDay))).isTrue();
+          },
+          'physical education day': function() {
+            if (currentYear == 2020) {
+              // for tokyo olympics special measures law
+              assertThat(isHoliday(new Date('2020/07/24'))).isTrue();
+            } else {
+              // 2nd monday of october
+              var dayDiff = dateInt.indexOf('monday') - new Date(currentYear + '/10/01').getDay();
+              var targetDay = 1 + dayDiff + (dayDiff < 0 ? 14 : 7);
+              assertThat(isHoliday(new Date(currentYear + '/10/' + targetDay))).isTrue();
+            }
+          },
+          'culture day': function() {
+            assertThat(isHoliday(new Date(currentYear + '/11/03'))).isTrue();
+          },
+          'labor thanksgiving day': function() {
+            assertThat(isHoliday(new Date(currentYear + '/11/23'))).isTrue();
           }
-        },
-        'culture day': function() {
-          assertThat(isHoliday(new Date(currentYear + '/11/03'))).isTrue();
-        },
-        'labor thanksgiving day': function() {
-          assertThat(isHoliday(new Date(currentYear + '/11/23'))).isTrue();
         }
-      }
-    },
-    'postMessage': function() {
-      // override UrlFetchApp.fetch
-      UrlFetchApp = {};
-      UrlFetchApp.fetch = function(url, params) {
-        return {
-          'url': url,
-          'params': params
+      },
+      'postMessage': function() {
+        // override UrlFetchApp.fetch
+        UrlFetchApp = {};
+        UrlFetchApp.fetch = function(url, params) {
+          return {
+            'url': url,
+            'params': params
+          };
         };
-      };
 
-      var testMessage = 'message';
+        var testMessage = 'message';
 
-      var result = JSON.stringify(postMessage(testMessage));
-      var expect = JSON.stringify({
-        'url': slackIncomingUrl,
-        'params': {
-          'method'     : 'post',
-          'contentType': 'application/json',
-          'payload'    : JSON.stringify({ 'text': testMessage })
-        }
-      });
+        var result = JSON.stringify(postMessage(testMessage));
+        var expect = JSON.stringify({
+          'url': slackIncomingUrl,
+          'params': {
+            'method'     : 'post',
+            'contentType': 'application/json',
+            'payload'    : JSON.stringify({ 'text': testMessage })
+          }
+        });
 
-      assertThat(result).is(expect);
+        assertThat(result).is(expect);
+      }
     }
   });
 }
