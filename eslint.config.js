e482496594e899656579e15b9e093b4e737574b9
jz4o <26317360+jz4o@users.noreply.github.com>
2024-07-18 23:31:44 +0900

    add eslint

---
+++ eslint.config.js
@@ -0,0 +1,44 @@
+import js from '@eslint/js';
+import googleAppsScript from 'eslint-plugin-googleappsscript';
+
+export default [
+  js.configs.recommended,
+  {
+    'ignores': [
+      'src/watchers/eset_watcher.js',
+      'src/watchers/jc3_watcher.js',
+      'src/watchers/jpcert_watcher.js',
+      'src/watchers/windows_forest_watcher.js',
+      'src/code.js',
+      'src/redmine.js',
+      'src/utilities.js',
+      'src/watcher.js',
+    ]
+  },
+  {
+    'languageOptions': {
+      'ecmaVersion': 2019,
+      'globals': {
+        ...googleAppsScript.environments.googleappsscript.globals
+      }
+    },
+    'rules': {
+      'arrow-parens': ['error', 'as-needed'],
+      'indent': ['error', 2],
+      'max-len': ['error', { 'code': 120 }],
+      'no-console': ['error'],
+      'no-ternary': ['off'],
+      'no-undef': ['off'],
+      'no-undefined': ['off'],
+      'no-unused-vars': ['off'],
+      'no-var': ['error'],
+      'object-curly-spacing': ['error', 'always'],
+      'one-var': ['error', 'never'],
+      'padded-blocks': ['error', 'never'],
+      'quotes': ['error', 'single'],
+      'quote-props': ['error', 'always'],
+      'semi': ['error', 'always']
+    }
+  }
+];
+
