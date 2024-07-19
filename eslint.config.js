5484978d14de985a9fabe49cd811cc3b595efc1c
jz4o <26317360+jz4o@users.noreply.github.com>
2024-07-19 10:02:14 +0900

    fix eslint offences in src/watchers/eset_watcher.js

--- eslint.config.js
+++ eslint.config.js
@@ -5,7 +5,6 @@ export default [
   js.configs.recommended,
   {
     'ignores': [
-      'src/watchers/eset_watcher.js',
       'src/watchers/jc3_watcher.js',
       'src/watchers/jpcert_watcher.js',
       'src/watchers/windows_forest_watcher.js',
